ccf9f356eb314898ff47e6cd451712a92d4d978d
chino <tnehd83@naver.com>
2025-02-28 13:13:25 +0900

    feat:테스트

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,6 @@ public class Main {
         System.out.println("test");
         System.out.println("upload");
         System.out.println("download");
+        System.out.println("test");
     }
 }
