da96383aa6c2ee318ff437d6693e7c5ce8aabc27
chino <tnehd83@naver.com>
2025-02-28 13:05:54 +0900

    feat:push

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,12 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Random;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-        Random random = new Random();
-
-        int randomNumber1 = random.nextInt(20, 45);
-        System.out.println(randomNumber1);
-
-        int randomNumber2 = random.nextInt(-128, 127);
-        System.out.println(randomNumber2);
+        System.out.println("test");
     }
 }
